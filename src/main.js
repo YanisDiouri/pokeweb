70ba62d5c3f452104196a4d1e1e400e8d8e303d0
Yanis Diouri El Jadidi <147667758+YanisDiouri@users.noreply.github.com>
2025-05-21 04:31:51 +0200

    Add: fetch pokemon from api

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <p>Hello World</p>
-  </div>
-`
+'use strict';
+
+const getPokemon = async () => {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=50");
+  const data = await response.json();
+  const pokeList = data.results;
+
+  pokeList.forEach(async (pokemon) => {
+    const dataPokemon = await fetch(pokemon.url);
+    const pokemonDetails = await dataPokemon.json();
+    console.log(pokemonDetails)
+
+  });
+};
+
+getPokemon();
