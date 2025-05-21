733fd72adc6b22ab896b723ef2c53073e689b282
Yanis Diouri El Jadidi <147667758+YanisDiouri@users.noreply.github.com>
2025-05-21 04:52:07 +0200

    Add: PokemonCard

--- src/main.js
+++ src/main.js
@@ -10,9 +10,27 @@ const getPokemon = async () => {
   pokeList.forEach(async (pokemon) => {
     const dataPokemon = await fetch(pokemon.url);
     const pokemonDetails = await dataPokemon.json();
-    console.log(pokemonDetails)
 
+
+    createPokemonCard(pokemonDetails);
   });
+};
+
+const createPokemonCard = (pokemon) => {
+  const card = document.createElement("div");
+  card.classList.add("pokemon-card");
+
+  const img = document.createElement("img");
+  img.src = pokemon.sprites.front_default;
+  img.alt = pokemon.name;
+
+  const name = document.createElement("h3");
+  name.textContent = pokemon.name.toUpperCase(); 
+
+  card.appendChild(img);
+  card.appendChild(name);
+
+  document.getElementById("pokemonList").appendChild(card);
 };
 
 getPokemon();
